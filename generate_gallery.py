0941464bf7c7ba3812e5f964a84eb68cfd3487f3
Michele Claus <michele.claus@eurac.edu>
2025-07-14 09:18:12 +0000

    fixing gallery

--- generate_gallery.py
+++ generate_gallery.py
@@ -412,24 +412,10 @@ title: {categories["tools"]["title"]}
 :columns: 1 1 2 3
 ```
 
-## SNAP
-
-```{{gallery-grid}}
-:category: snap
-:columns: 1 1 2 3
-```
-
 ## STAC
 
 ```{{gallery-grid}}
 :category: stac
-:columns: 1 1 2 3
-```
-
-## Zarr
-
-```{{gallery-grid}}
-:category: zarr
 :columns: 1 1 2 3
 ```
 
